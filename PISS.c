c74ff219600654f29ea8d0b6d6cebc09f163478b
iigo <narf.masterlord@gmail.com>
2022-11-15 19:08:41 -0500

    Changed Commenting Style

--- PISS.c
+++ PISS.c
@@ -1,23 +1,30 @@
-// Periodic Immersive SteamVR Screenshots is a simple utility that utilizes the OpenVR api to take a
-// SteamVR screenshot every hour on the top of the hour.
+/*
+ * Periodic Immersive SteamVR Screenshots is a simple utility that utilizes the OpenVR api to take a
+ * SteamVR screenshot every hour on the top of the hour.
+ */
 
-// System headers for any extra stuff we need.
+/* System headers for any extra stuff we need. */
 #include <stdbool.h>
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
 
-// Include CNFG (rawdraw) for generating a window and/or OpenGL context.
-// Included to manage windows header files, but may be used more explicitly in the future.
+/* 
+ * Include CNFG (rawdraw) for generating a window and/or OpenGL context.
+ * Included to manage windows header files, but may be used more explicitly in the future.
+ */
 #define CNFG_IMPLEMENTATION
 #define CNFGOGL
 #include "rawdraw_sf.h"
 
-// Include OpenVR header so we can interact with VR stuff.
+/* Include OpenVR header so we can interact with VR stuff. */
 #undef EXTERN_C
 #include "openvr_capi.h"
 
-// OpenVR Doesn't define these for some reason (I don't remember why) so we define the functions here. They are copy-pasted from the bottom of openvr_capi.h
+/*
+ * OpenVR Doesn't define these for some reason (I don't remember why) so we
+ * define the functions here. They are copy-pasted from the bottom of openvr_capi.
+ */
 intptr_t VR_InitInternal( EVRInitError *peError, EVRApplicationType eType );
 void VR_ShutdownInternal();
 bool VR_IsHmdPresent();
@@ -26,13 +33,13 @@ bool VR_IsRuntimeInstalled();
 const char * VR_GetVRInitErrorAsSymbol( EVRInitError error );
 const char * VR_GetVRInitErrorAsEnglishDescription( EVRInitError error );
 
-// These are functions that rawdraw calls back into.
+/* These are functions that rawdraw calls back into. */
 void HandleKey( int keycode, int bDown ) { }
 void HandleButton( int x, int y, int button, int bDown ) { }
 void HandleMotion( int x, int y, int mask ) { }
 void HandleDestroy() { }
 
-// This function was copy-pasted from cnovr.
+/* This function was copy-pasted from cnovr. */
 void * CNOVRGetOpenVRFunctionTable( const char * interfacename )
 {
 	EVRInitError e;
@@ -47,7 +54,7 @@ void * CNOVRGetOpenVRFunctionTable( const char * interfacename )
 	return ret;
 }
 
-// These are interfaces into OpenVR, they are basically function call tables.
+/* These are interfaces into OpenVR, they are basically function call tables. */
 //struct VR_IVRSystem_FnTable * oSystem;
 //struct VR_IVROverlay_FnTable * oOverlay;
 struct VR_IVRApplications_FnTable * oApplications;
@@ -57,8 +64,10 @@ struct VR_IVRScreenshots_FnTable * oScreenshots;
 
 int main()
 {
-    // We put this in a codeblock because it's logically together.
-	// no reason to keep the token around.
+    /* 
+	 * We put this in a codeblock because it's logically together. 
+	 * no reason to keep the token around.
+	 */
 	{
 		EVRInitError ierr;
 		uint32_t token = VR_InitInternal( &ierr, EVRApplicationType_VRApplication_Overlay );
@@ -68,9 +77,11 @@ int main()
 			return -5;
 		}
 
-		// Get the system and overlay interfaces.  We pass in the version of these
-		// interfaces that we wish to use, in case the runtime is newer, we can still
-		// get the interfaces we expect.
+		/* 
+		 * Get the system and overlay interfaces.  We pass in the version of these
+		 * interfaces that we wish to use, in case the runtime is newer, we can still
+		 * get the interfaces we expect.
+		 */
 		//oSystem = CNOVRGetOpenVRFunctionTable( IVRSystem_Version );
 		//oOverlay = CNOVRGetOpenVRFunctionTable( IVROverlay_Version );
 		oApplications = CNOVRGetOpenVRFunctionTable( IVRApplications_Version );
@@ -78,6 +89,7 @@ int main()
 		//oInput = CNOVRGetOpenVRFunctionTable( IVRInput_Version );
 	}
 
+	/* Ensure that the application is registered in SteamVR so you can setup autolaunch */
 	{
 		if (!oApplications->IsApplicationInstalled("iigo.PISS"))
 		{
@@ -87,10 +99,10 @@ int main()
 			char fname[_MAX_FNAME];
 			char ext[_MAX_EXT];
 
-			// Gets the path of the exe file
+			/* Gets the path of the exe file */
 			GetModuleFileName( NULL, path_buffer, sizeof(path_buffer));
 			_splitpath( path_buffer, drive, dir, fname, ext );
-			_makepath( path_buffer, drive, dir, NULL, NULL ); // removes the file name and extension from the buffer
+			_makepath( path_buffer, drive, dir, NULL, NULL ); /* removes the file name and extension from the buffer */
 			strncat(path_buffer, "PISS.vrmanifest", sizeof("PISS.vrmanifest"));
 			EVRApplicationError app_error;
 			app_error = oApplications->AddApplicationManifest(path_buffer, false);
@@ -100,21 +112,21 @@ int main()
 	}
 
 	time_t now = time(NULL);
-	//time_t now = 1667707200; // 2022 Nov 6th at midnight
-	//time_t now = 1678597200; // 2023 Mar 12th at midnight
+	//time_t now = 1667707200; /* 2022 Nov 6th at midnight */
+	//time_t now = 1678597200; /* 2023 Mar 12th at midnight */
 	int hoursSinceEpoch = now / 3600;
 	int sshour = hoursSinceEpoch;
 
     while( true )
     {
 
-		//now = now + 1; //increase time by one second each while loop
+		//now = now + 1; /* increase time by one second each while loop */
 		now = time(NULL);
 		
 		hoursSinceEpoch = now / 3600;
 		struct tm *tm_struct = gmtime(&now);
 
-		// check to see if its a new hour
+		/* check to see if its a new hour */
 		if (sshour != hoursSinceEpoch)
 		{
 
@@ -125,10 +137,10 @@ int main()
 			char ext[_MAX_EXT];
 
 
-			// Gets the path of the exe file
+			/* Gets the path of the exe file */
 			GetModuleFileName( NULL, path_buffer, sizeof(path_buffer));
 			_splitpath( path_buffer, drive, dir, fname, ext );
-			_makepath( path_buffer, drive, dir, NULL, NULL ); // removes the file name and extension from the buffer
+			_makepath( path_buffer, drive, dir, NULL, NULL ); /* removes the file name and extension from the buffer */
 			char ssFolderName[] = "Screenshots\\";
 			strncat(path_buffer, ssFolderName, sizeof(ssFolderName));
 			CreateDirectory(path_buffer, NULL);
@@ -137,7 +149,7 @@ int main()
 			strncat(path_buffer, ssMonthFolder, sizeof ssMonthFolder);
 			CreateDirectory(path_buffer, NULL);
 
-			char timestamp[] = "2011-10-08_07-07-09";
+			char timestamp[] = "1970-01-01_00-00-00";
 			char screenshotpath[sizeof path_buffer + sizeof timestamp];
 			strcpy(screenshotpath, path_buffer);
 			strftime(timestamp, sizeof timestamp, "%Y-%m-%d_%H-%M-%S", tm_struct);
@@ -156,7 +168,7 @@ int main()
 
 		}
 
-		Sleep( 500 ); // sleep for half a second (500ms)
+		Sleep( 500 ); /* sleep for half a second (500ms) */
     }
 
 	return 0;
